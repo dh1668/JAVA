c274f8accf0b6d638fba16df4ae77443e80b9cf3
dh1668 <102003945+dh1668@users.noreply.github.com>
2022-03-30 21:58:30 +0900

    Add files via upload

---
+++ 0329/기초조지자/No_2.java
@@ -0,0 +1,18 @@
+package 기초조지자;
+
+public class No_2 {
+
+	public static void main(String[] args) {
+//		★ 반복문
+		int sum, i = 0;
+		while(i < 10) {
+			sum += ;
+		System.out.println(sum);
+			i++;
+		}
+		
+		
+		
+	}
+
+}
